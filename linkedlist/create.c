a2d4b3db9f6ad86a4941a9524dd916c42f77296c
Prathamesh4519 <prathamesh4519@gmail.com>
2025-03-02 22:04:31 +0530

    first try...

---
+++ linkedlist/create.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+struct listnode {
+    int data;
+    struct listnode* next;
+};
+typedef struct listnode list;
+
+
+
+void create(list* ptr) {
+   
+    while(1) {
+        printf("Enter the value\n");
+        int value;
+        scanf("%d",&value);
+        ptr->data=value;
+        printf("Do you want to continue?0/1\n");
+        int choice;
+        scanf("%d",&choice);
+        if(choice==1) {
+            ptr->next=(list*)malloc(sizeof(list));
+            
+            if (ptr->next == NULL) {
+                printf("Memory allocation failed.\n");
+                exit(0);
+            }
+            ptr=ptr->next;
+        } else {
+            ptr->next=NULL;
+            break;
+        }
+    }
+}
+
+void print(list* ptr) {
+    while(ptr!=NULL) {
+        printf("%d,",ptr->data);
+        ptr=ptr->next;
+    }
+    printf("\n");
+}
+
+
+int main() {
+    list* head=(list*)malloc(sizeof(list));
+    if(head==NULL) {
+        printf("Error,No space availed\n");
+        exit(0);
+    }
+    create(head);
+    print(head);
+    return 0;
+}
